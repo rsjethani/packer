076de70d73e603f06d5073ee967cb68dc01df769
Adrien Delorme <adrien.delorme@icloud.com>
2019-01-21 18:07:51 +0100

    add race detection test

--- packer/progressbar_test.go
+++ packer/progressbar_test.go
@@ -1,9 +1,13 @@
 package packer
 
 import (
+	"bytes"
+	"io/ioutil"
+	"testing"
 	"time"
 
 	"github.com/cheggaaa/pb"
+	"golang.org/x/sync/errgroup"
 )
 
 func speedyProgressBar(bar *pb.ProgressBar) {
@@ -11,4 +15,24 @@ func speedyProgressBar(bar *pb.ProgressBar) {
 	bar.SetRefreshRate(1 * time.Millisecond)
 	bar.NotPrint = true
 	bar.Format("[\x00=\x00>\x00-\x00]")
+}
+func TestProgressTracking_races(t *testing.T) {
+	bar := defaultUiProgressBar
+	g := errgroup.Group{}
+	txt := []byte("foobarbaz dolores")
+	b := bytes.NewReader(txt)
+
+	for i := 0; i < 100; i++ {
+		g.Go(func() error {
+			tracker := bar.TrackProgress("file,", 1, 42, ioutil.NopCloser(b))
+
+			b := []byte("i")
+			for i := 0; i < 42; i++ {
+				tracker.Read(b)
+			}
+			return nil
+		})
+	}
+
+	g.Wait()
 }
